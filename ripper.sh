37f8d32873ff7bbfe8eddb6aa787fab9cb10e032
yzerlaut <yann.zerlaut@gmail.com>
2020-09-07 15:02:09 +0200

    FIX adding cover-art download

--- ripper.sh
+++ ripper.sh
@@ -78,6 +78,12 @@ do
       tracknumber=$(grep -o -P '.{0,0}track_number.{0,7}' tmp.html | cut -d: -f2 | cut -d, -f1)
       echo "Track number = $tracknumber"
       # Fetch the covert-art
+      string=$(grep -o -P '.{0,0}background-image:url.{0,200}' tmp.html | cut -d"(" -f2 | cut -d")" -f1)
+      to_replace="//"
+      empty=""
+      string=${string/$to_replace/$empty}
+      echo $string
+      wget -q -O cover.jpg "$string"
     fi
   fi
 done
--- trackify.sh
+++ trackify.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-TN=$(grep -o -P '.{0,0}background-image:url(.{0,200}' tmp.html | cut -d: -f2 | cut -d, -f1)
-echo $TN  
-
-# string=$(echo "$1" | cut -d: -f3)
-# string="http://open.spotify.com/track/$string"
-# wget -q -O tmp.html "$string"
-
-# # Echo out track number
-# grep open.spotify.com/track tmp.html | cut -d\" -f4 | \
-#   awk "{if (\$0 == \"$string\") {print NR}}"
-
-# string=$(grep background: tmp.html | cut '-d/' -f 3,4,5 | cut '-d)' -f1)
-# wget -q -O cover.jpg "$string"
-
-rm -f tmp.html
+string=$(grep -o -P '.{0,0}background-image:url.{0,200}' tmp.html | cut -d"(" -f2 | cut -d")" -f1)
+to_replace="//"
+empty=""
+string=${string/$to_replace/$empty}
+echo $string
+wget -q -O cover.jpg "$string"
